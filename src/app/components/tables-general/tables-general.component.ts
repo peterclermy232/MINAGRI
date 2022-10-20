993e07fe10db9338529ca98f812592eaa162bb7c
peterclermy232 <peterclermy232@gmail.com>
2022-10-20 04:13:01 +0300

    created Role screen

--- src/app/components/tables-general/tables-general.component.ts
+++ src/app/components/tables-general/tables-general.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-tables-general',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TablesGeneralComponent implements OnInit {
 
-  constructor() { }
+  formValue !:FormGroup;
+  //employeeModelObj : EmployeeModel = new EmployeeModel();
+  employeeData !:any;
+  showAdd!: boolean;
+  showUpdate!:boolean;
+  constructor(private formbuilder:FormBuilder,
+    ) { }
 
   ngOnInit(): void {
+    this.formValue = this.formbuilder.group({
+      name : [''],
+      description : ['']
+      //email : [''],
+     // mobile : [''],
+     // salary : ['']
+    })
+  
   }
 
 }
