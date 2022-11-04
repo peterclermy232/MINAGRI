264f8d0511847ef7eb1989a287cecc1fd59e2373
endungu <endungu@jambopay.com>
2022-11-04 19:44:48 +0300

    fix:oneendpoint

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,6 @@ export const environment: ENV = {
   production: true,
   consumer_key: 'phaIRZmh7WjBIae1VxcL_zMcjlca',
   consumer_secret: 'FyoQGDIrMeNY0VERT4isrjI5Bq0a',
-  crop_baseurl:
-    'https://apim-minagri.wrightinteractives.com:8243/crop-insurance/1.0',
-  oauth_baseurl: 'https://apim-minagri.wrightinteractives.com:9443',
+  crop_baseurl: 'https://apim-minagri.wrightinteractives.com',
+  oauth_baseurl: 'https://apim-minagri.wrightinteractives.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,9 +8,8 @@ export const environment: ENV = {
   production: false,
   consumer_key: 'phaIRZmh7WjBIae1VxcL_zMcjlca',
   consumer_secret: 'FyoQGDIrMeNY0VERT4isrjI5Bq0a',
-  crop_baseurl:
-    'https://apim-minagri.wrightinteractives.com:8243/crop-insurance/1.0',
-  oauth_baseurl: 'https://apim-minagri.wrightinteractives.com:9443',
+  crop_baseurl: 'https://apim-minagri.wrightinteractives.com',
+  oauth_baseurl: 'https://apim-minagri.wrightinteractives.com',
 };
 
 /*
