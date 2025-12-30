9afce7aa0a853a1836d334cb71926bf9a9e54220
Peter Atito <peterclermy232@gmail.com>
2025-12-30 10:51:59 +0300

    Add Netlify configuration files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,18 +1,18 @@
+// src/environments/environment.ts
 import { ENV } from '../app/types';
 
 export const environment: ENV = {
+  production: false,
   // Django API configuration
-  apiUrl: 'http://127.0.0.1:8001/api/v1',
-  authUrl: 'http://127.0.0.1:8001/api/v1/auth',
-
-  production: true,
+  apiUrl: 'https://web-production-39ac0.up.railway.app/api/v1',
+  authUrl: 'https://web-production-39ac0.up.railway.app/api/v1/auth',
+// 8000
+  // Legacy fields (keep for compatibility)
   consumer_key: 'hpHBZ6V_SxZUzeJX2VuwPnpLwb4a',
   consumer_secret: '6psbCCyfbTi3rrN0ybApNr4C2Wka',
   user_consumer_key: 'Eric',
   user_consumer_secret: 'w3lc0m3',
-  // crop_baseurl: 'https://apim-minagri.wrightinteractives.com',
-  crop_baseurl: 'https://crop-insurance-minagri.wrightinteractives.com',
-  auth_baseurl:
-    'https://apim-minagri.wrightinteractives.com/authentication/1.0.0',
-  oauth_baseurl: 'https://apim-minagri.wrightinteractives.com',
+  crop_baseurl: 'https://web-production-39ac0.up.railway.app/api/v1',
+  auth_baseurl: 'https://web-production-39ac0.up.railway.app/api/v1',
+  oauth_baseurl: 'https://web-production-39ac0.up.railway.app/api/v1',
 };
